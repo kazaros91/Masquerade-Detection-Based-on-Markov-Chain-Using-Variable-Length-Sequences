fde362871bb701211fe4bf67d2a7ea187fb966b3
kazaros91 <33250928+kazaros91@users.noreply.github.com>
2019-09-30 11:57:55 +0400

    Update VariableLengthMethod.java

--- src/methods/VariableLengthMethod.java
+++ src/methods/VariableLengthMethod.java
@@ -6,7 +6,6 @@ import detection.data.DetectionResult;
 
 public interface VariableLengthMethod<T> {
 	
-	public void filterProfile();	// filter out low-recognizable patterns
 	public void train(List<T> trainingData);
 	public DetectionResult detect(List<String> detectionData, double threshold);
 	public DetectionResult detect(List<String> truePositiveData);
